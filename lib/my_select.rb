7ea3a20a570d5b6021845ecba90a66cc915d3d50
davidtspf <david.tannenbaum@gmail.com>
2017-08-10 17:48:47 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+  i = 0
+
+  new_array = []
+
+  while i < collection.length
+    if yield(collection[i]) == true
+      new_array.push(collection[i])
+    end
+    i += 1
+  end
+
+  return new_array
+
+end
+
+cool_nums = [1, 2, 3, 4]
+my_select(cool_nums) do |nums|
+  nums.even?
 end
